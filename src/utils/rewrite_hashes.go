cdb51174b5fdce06200b10d953cf125188f72a8e
Peter Ebden <peter.ebden@gmail.com>
2017-04-03 21:08:46 +0100

    starting on hash rewriting for plz hash

---
+++ src/utils/rewrite_hashes.go
@@ -0,0 +1,8 @@
+package utils
+
+import "core"
+
+// RewriteHashes is a stub used during initial bootstrap.
+func RewriteHashes(state *core.BuildState, name string, hashes map[string]string) {
+	// Build the
+}
