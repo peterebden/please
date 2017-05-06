e932b7585266496602d913b33714083581b7eb4f
Peter Ebden <peter.ebden@gmail.com>
2017-05-06 21:28:41 +0100

    adding some basic rules

---
+++ test/js_rules/lib.js
@@ -0,0 +1,7 @@
+// Basic JS library to test how it all fits together.
+
+function getAnswer(a, b) {
+    return a === 6 && b === 9 ? 42 : a * b;
+}
+
+module.exports = getAnswer;
---
+++ test/js_rules/test.spec.js
@@ -0,0 +1,5 @@
+const lib = require('test/js_rules/lib');
+
+test('the answer to life, the universe, and everything', () => {
+    expect(sum(6, 9)).toBe(42);
+});
