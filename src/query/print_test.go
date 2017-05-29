4082b0545982ee9684c6d311a34e6c84e9297667
Peter Ebden <peter.ebden@gmail.com>
2017-05-29 12:35:56 +0100

    extending test

--- src/query/print_test.go
+++ src/query/print_test.go
@@ -4,8 +4,13 @@
 package query
 
 import (
+	"bytes"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"core"
 )
@@ -69,5 +74,124 @@ func TestAllFieldsArePresentAndAccountedFor(t *testing.T) {
 	}
 }
 
-func TestPrintBasic(t *testing.T) {
+func TestPrintOutput(t *testing.T) {
+	target := core.NewBuildTarget(core.ParseBuildLabel("//src/query:test_print_output", ""))
+	target.AddSource(src("file.go"))
+	target.AddSource(src(":target1"))
+	target.AddSource(src("//src/query:target2"))
+	target.AddSource(src("//src/query:target3|go"))
+	target.AddSource(src("//src/core:core"))
+	target.AddOutput("out1.go")
+	target.AddOutput("out2.go")
+	target.Command = "cp $SRCS $OUTS"
+	target.Tools = append(target.Tools, src("//tools:tool1"))
+	target.IsBinary = true
+	s := testPrint(target)
+	expected := `  build_rule(
+      name = 'test_print_output',
+      srcs = [
+          'file.go',
+          '//src/query:target1',
+          '//src/query:target2',
+          '//src/query:target3|go',
+          '//src/core:core',
+      ],
+      outs = [
+          'out1.go',
+          'out2.go',
+      ],
+      cmd = 'cp $SRCS $OUTS',
+      binary = True,
+      tools = [
+          '//tools:tool1',
+      ],
+  )
+
+`
+	assert.Equal(t, expected, s)
+}
+
+func TestFilegroupOutput(t *testing.T) {
+	target := core.NewBuildTarget(core.ParseBuildLabel("//src/query:test_filegroup_output", ""))
+	target.AddSource(src("file.go"))
+	target.AddSource(src(":target1"))
+	target.IsFilegroup = true
+	target.Visibility = core.WholeGraph
+	s := testPrint(target)
+	expected := `  filegroup(
+      name = 'test_filegroup_output',
+      srcs = [
+          'file.go',
+          '//src/query:target1',
+      ],
+      visibility = [
+          'PUBLIC',
+      ],
+  )
+
+`
+	assert.Equal(t, expected, s)
+}
+
+func TestTestOutput(t *testing.T) {
+	target := core.NewBuildTarget(core.ParseBuildLabel("//src/query:test_test_output", ""))
+	target.AddSource(src("file.go"))
+	target.IsTest = true
+	target.IsBinary = true
+	target.BuildTimeout = 30 * time.Second
+	target.TestTimeout = 60 * time.Second
+	target.Flakiness = 2
+	s := testPrint(target)
+	expected := `  build_rule(
+      name = 'test_test_output',
+      srcs = [
+          'file.go',
+      ],
+      binary = True,
+      test = True,
+      flaky = 2,
+      timeout = 30,
+      test_timeout = 60,
+  )
+
+`
+	assert.Equal(t, expected, s)
+}
+
+func TestPostBuildOutput(t *testing.T) {
+	target := core.NewBuildTarget(core.ParseBuildLabel("//src/query:test_post_build_output", ""))
+	target.PostBuildFunction = 1
+	target.AddCommand("opt", "/bin/true")
+	target.AddCommand("dbg", "/bin/false")
+	s := testPrint(target)
+	expected := `  build_rule(
+      name = 'test_post_build_output',
+      cmd = {
+          'opt': '/bin/true',
+          'dbg': '/bin/false',
+      },
+      post_build = '<python ref>',
+  )
+
+`
+	assert.Equal(t, expected, s)
+}
+
+func testPrint(target *core.BuildTarget) string {
+	var buf bytes.Buffer
+	p := printer{w: &buf}
+	p.queryPrint(target)
+	return buf.String()
+}
+
+func src(in string) core.BuildInput {
+	const pkg = "src/query"
+	if strings.HasPrefix(in, "//") || strings.HasPrefix(in, ":") {
+		src, err := core.TryParseNamedOutputLabel(in, pkg)
+		if err != nil {
+			panic(err)
+		}
+		return src
+	}
+	return core.FileLabel{File: in, Package: pkg}
 }
