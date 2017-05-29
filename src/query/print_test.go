0c67659c5b7fddbc611f418baaf8d2caf0ce5705
Peter Ebden <peter.ebden@gmail.com>
2017-05-29 21:34:59 +0100

    Remove old test; we can now determine from the runtime code whether we know about all fields.

--- src/query/print_test.go
+++ src/query/print_test.go
@@ -1,11 +1,7 @@
-// Test to make sure we don't forget about adding new fields to print
-// (because I keep doing that...)
-
 package query
 
 import (
 	"bytes"
-	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -15,63 +11,12 @@ import (
 	"core"
 )
 
-// Add fields to this list *after* you teach print about them.
-var KnownFields = map[string]bool{
-	"BuildTimeout":                true,
-	"BuildingDescription":         true,
-	"Command":                     true,
-	"Commands":                    true,
-	"Containerise":                true,
-	"ContainerSettings":           true,
-	"Data":                        true,
-	"dependencies":                true,
-	"Flakiness":                   true,
-	"Hashes":                      true,
-	"IsBinary":                    true,
-	"IsTest":                      true,
-	"IsFilegroup":                 true,
-	"Label":                       true, // this includes the target's name
-	"Labels":                      true,
-	"Licences":                    true,
-	"namedOutputs":                true,
-	"NamedSources":                true,
-	"NeedsTransitiveDependencies": true,
-	"NoTestOutput":                true,
-	"OptionalOutputs":             true,
-	"OutputIsComplete":            true,
-	"outputs":                     true,
-	"PreBuildFunction":            true,
-	"PostBuildFunction":           true,
-	"Provides":                    true,
-	"Requires":                    true,
-	"Sources":                     true,
-	"Stamp":                       true,
-	"TestCommand":                 true,
-	"TestCommands":                true,
-	"TestOnly":                    true,
-	"TestOutputs":                 true,
-	"TestTimeout":                 true,
-	"Tools":                       true,
-	"Visibility":                  true,
-
-	// These aren't part of the declaration, only used internally.
-	"state":         true,
-	"Results":       true,
-	"PreBuildHash":  true,
-	"PostBuildHash": true,
-	"RuleHash":      true,
-	"mutex":         true,
-}
-
 func TestAllFieldsArePresentAndAccountedFor(t *testing.T) {
 	target := core.BuildTarget{}
-	val := reflect.ValueOf(target)
-	for i := 0; i < val.Type().NumField(); i++ {
-		field := val.Type().Field(i)
-		if !KnownFields[field.Name] {
-			t.Errorf("Unaccounted field in 'query print': %s", field.Name)
-		}
-	}
+	var buf bytes.Buffer
+	p := newPrinter(&buf, &target, 0)
+	p.PrintTarget()
+	assert.False(t, p.error, "Appears we do not know how to print some fields")
 }
 
 func TestPrintOutput(t *testing.T) {
