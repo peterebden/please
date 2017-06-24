711a42e0b3d960fc4c44e42f0ea3c154664f1db3
Peter Ebden <peter.ebden@gmail.com>
2017-06-24 22:54:04 +0100

    throwing some code around, not sure how anything works any more

--- tools/misc/plz_complete.zsh
+++ tools/misc/plz_complete.zsh
@@ -1,35 +1,45 @@
-#compdef plz
-#autoload
-
 ####################################################
 # plz zsh completion
 #
-# rename to _plz and put somewhere in your $fpath
-# e.g. /usr/local/share/zsh/site-functions
+# add source <(plz query completion script) to your .zshrc
+# to activate this.
 ####################################################
 
-local expl
-local arguments
-local -a _1st_arguments arguments
-local options
+_plz() {
+    local word completions
+    word="${@[-1]}"
 
-_1st_arguments=("${(f)$(plz --help \
-                        | perl -lnE 'say if (/Available commands/...//)' \
-                        | grep '^ ' \
-                        | perl -pE 's/^ +//; s/(?<=[^\s])\s+/:/')}")
+    completions="$(vault --cmplt "${word}")"
+    reply=( "${(ps:\n:)completions}" )
 
-_targets() {
-    completions=("${(f)$(plz query completions --cmd $words[2,-1])}")
-    _wanted completions expl "Target" compadd -a completions
+  if [[ "$word" == -* || "$COMP_CWORD" == "1" || ("$COMP_CWORD" == "2" && "${COMP_WORDS[1]}" == "query")]]; then
+      COMPREPLY=( $( compgen -W "`GO_FLAGS_COMPLETION=1 plz ${word}`" -- $word) )
+  else
+      COMPREPLY=( $( compgen -W "`plz --noupdate -p query completions --cmd ${COMP_WORDS[1]} $cur 2>/dev/null`" -- $cur ) )
+  fi
+
+
+    local expl
+    local arguments
+    local -a _1st_arguments arguments
+    local options
+
+    _1st_arguments=("${(f)$(plz --help \
+                            | perl -lnE 'say if (/Available commands/...//)' \
+                            | grep '^ ' \
+                            | perl -pE 's/^ +//; s/(?<=[^\s])\s+/:/')}")
+
+    if [[ $words[-1] =~ '-' ]]; then
+        options=("${(f)$(${words[1,-2]} --help \
+                          | perl -lnE 'if (/ *(-[a-zA-Z]), ([^ =]*)=? *(.+)/) {say "${1}:$3\n${2}:$3"} elsif (/^ *(--[^ =]*)[ =]*(.*)/) {say "${1}[$2]"}')}")
+        _arguments ${options[@]} '1:'
+    elif (( CURRENT == 2 )); then
+      _describe -t commands "plz subcommand" _1st_arguments
+      return
+    elif [[ $words[-2] =~ '^[a-z]' && $words[-2] != 'update' ]]; then
+        completions=("${(f)$(plz query completions --cmd $words[2,-1])}")
+        _wanted completions expl "Target" compadd -a completions
+    fi
 }
 
-if [[ $words[-1] =~ '-' ]]; then
-    options=("${(f)$(${words[1,-2]} --help \
-                      | perl -lnE 'if (/ *(-[a-zA-Z]), ([^ =]*)=? *(.+)/) {say "${1}:$3\n${2}:$3"} elsif (/^ *(--[^ =]*)[ =]*(.*)/) {say "${1}[$2]"}')}")
-    _arguments ${options[@]} '1:'
-elif (( CURRENT == 2 )); then
-  _describe -t commands "plz subcommand" _1st_arguments
-  return
-elif [[ $words[-2] =~ '^[a-z]' && $words[-2] != 'update' ]]; then
-    _targets
-fi
+compctl -K _plz plz
