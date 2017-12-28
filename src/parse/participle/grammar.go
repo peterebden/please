eed270f169a9405c7c889598352848456ede5608
Peter Ebden <peter.ebden@gmail.com>
2017-12-28 13:31:08 +0000

    working on grammar again

--- src/parse/participle/grammar.go
+++ src/parse/participle/grammar.go
@@ -2,19 +2,27 @@ package participle
 
 // A fileInput is the top-level structure of a BUILD file.
 type fileInput struct {
-	Statements []*statement `@@`
+	Statements []*statement `{ @@ }`
+	EOF        string       `@EOF`
 }
 
 // A statement is the type we work with externally the most; it's a single Python statement.
 type statement struct {
-	FuncDef *funcDef `"def" @@ ":"`
+	FuncDef *funcDef `"def" @@`
 }
 
 type funcDef struct {
-	Name      string     `@Ident`
-	Arguments []argument `"(" @@ ")"`
+	Name      string      `@Ident`
+	Arguments []*argument `"(" [ @@ { "," @@ } ] ")" ":"`
+	//	Statements []*statement `@@`
+	End string `@Unindent`
 }
 
 type argument struct {
-	Name string `@Ident`
+	Name    string `@Ident`
+	Default struct {
+		Ident  string `  @Ident`
+		String string `| @String`
+		Int    int    `| @Int`
+	} `[ "=" @@ ]`
 }
