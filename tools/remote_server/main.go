4262eab9dfad06e34a83e16aaccf056e12100955
Peter Ebden <peter.ebden@gmail.com>
2018-09-17 10:00:30 +0100

    moar

--- tools/remote_server/main.go
+++ tools/remote_server/main.go
@@ -23,6 +23,7 @@ var opts = struct {
 		Master cli.URL `short:"m" long:"master" required:"true" description:"URL of the master to connect to"`
 		Name   string  `short:"n" long:"name" description:"Name of this worker instance."`
 		URL    cli.URL `short:"u" long:"url" required:"true" description:"Public URL of this instance"`
+		Dir    string  `short:"d" long:"dir" default:"." description:"Working directory to run tests in"`
 	} `command:"worker" description:"Starts this server as a worker"`
 }{
 	Usage: `
@@ -41,6 +42,6 @@ func main() {
 		master.Start(opts.Port)
 	} else {
 		log.Notice("Starting as a worker, connecting to master at %s", opts.Worker.Master)
-		worker.Connect(opts.Worker.Master.String(), opts.Worker.Name, opts.Worker.URL.String(), opts.Port)
+		worker.Connect(opts.Worker.Master.String(), opts.Worker.Name, opts.Worker.URL.String(), opts.Port, opts.Worker.Dir)
 	}
 }
--- tools/remote_server/worker/worker.go
+++ tools/remote_server/worker/worker.go
@@ -2,13 +2,15 @@ package worker
 
 import (
 	"context"
-	"io"
-	"sync"
+	"io/ioutil"
+	"path"
+	"sort"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
 	"gopkg.in/op/go-logging.v1"
 
+	"core"
 	"grpcutil"
 	pb "test/proto/remote"
 )
@@ -17,10 +19,10 @@ var log = logging.MustGetLogger("worker")
 
 // Connect connects to the master and receives messages.
 // It continues forever until the server terminates.
-func Connect(master, name, url string, port int) {
+func Connect(master, name, url string, port int, dir string) {
 	// Start the local gRPC server first.
-	w := &worker{}
-	go w.start(port)
+	w := &worker{Dir: path.Join(dir, "test")}
+	go w.Start(port)
 
 	conn, err := grpc.Dial(master, grpc.WithInsecure(),
 		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(grpc_retry.WithMax(3))))
@@ -40,11 +42,19 @@ func Connect(master, name, url string, port int) {
 		log.Fatalf("Failed to connect: %s", err)
 	}
 	worker.Client = client
-
+	// We won't receive a response until the server stops.
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Stream terminated: %s", err)
+	}
+	// If we get here it's a non-fatal response so we exit gently.
+	log.Notice("Stream terminated: %s", resp.Error)
 }
 
 type worker struct {
-	Client pb.RemoteTestMasterClient
+	Dir     string
+	Client  pb.RemoteTestMasterClient
+	Running bool
 }
 
 // Start starts serving the worker gRPC server on the given port.
@@ -52,4 +62,76 @@ func (w *worker) Start(port int) {
 	s := grpc.NewServer()
 	pb.RegisterRemoteTestWorkerServer(s, w)
 	grpcutil.StartServer(s, port)
+}
+
+// ExecuteTest implements the RemoteTestWorker RPC service.
+func (w *worker) ExecuteTest(ctx context.Context, req *pb.RemoteTestRequest) (*pb.RemoteTestResponse, error) {
+	if w.Running {
+		return &pb.RemoteTestResponse{Error: "worker busy"}, nil
+	}
+	// We could do this with a mutex or whatever, but we don't want callers to wait; this
+	// should never happen (because the master should arbitrate the requests).
+	w.Running = true
+	defer func() {
+		w.Running = false
+	}()
+	code, stderr, err := w.runTest(ctx, req)
+	if err != nil {
+		return &pb.RemoteTestResponse{Error: err.Error()}, nil
+	}
+	return &pb.RemoteTestResponse{
+		ExitCode: int32(code),
+		Results:  w.findResults(req),
+		Stderr:   stderr,
+	}
+}
+
+// runTest runs a single test.
+func (w *worker) runTest(ctx context.Context, req *pb.RemoteTestRequest) (int, []byte, error) {
+	if err := os.RemoveAll(w.Dir); err != nil {
+		return 0, nil, err
+	} else if err := os.MkdirAll(w.Dir, os.ModeDir|0755); err != nil {
+		return 0, nil, err
+	} else if err := w.setupFiles(req.Outputs, req.Data); err != nil {
+		return 0, nil, err
+	}
+	// Using a default state here is not really accurate but is the best we can do for now.
+	// TODO(peterebden): Find some way of specifying this on the worker.
+	target := core.NewBuildTarget(core.BuildLabel{
+		PackageName: req.Label.PackageName,
+		Name:        req.Label.Name,
+	})
+	// Set up target files - these become env vars later.
+	for _, output := range orderedFiles(req.Outputs) {
+		target.AddOutput(output)
+	}
+	for _, datum := range orderedFiles(req.Data) {
+		target.AddDatum(core.FileLabel{File: datum})
+	}
+	state := core.DefaultBuildState()
+	state.NeedCoverage = req.Coverage
+	env := core.TestEnvironment(state, target, w.Dir)
+
+}
+
+// setupFiles creates the files in the test directory.
+func (w *worker) setupFiles(files ...map[string][]byte) error {
+	for _, m := range files {
+		for filename, contents := range m {
+			if err := ioutil.WriteFile(path.Join(w.Dir, filename), contents, 0755); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// orderedFiles returns the keys of one of the proto file maps, in order.
+func (w *worker) orderedFiles(in map[string][]byte) []string {
+	ret := make([]string, 0, len(in))
+	for file := range in {
+		ret = append(ret, file)
+	}
+	sort.Strings(ret)
+	return ret
 }
