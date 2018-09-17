e7a2dde46ae2b589a34ee1269783c2bc569dc191
Peter Ebden <peter.ebden@gmail.com>
2018-09-17 10:28:46 +0100

    moar moar

--- tools/remote_server/worker/worker.go
+++ tools/remote_server/worker/worker.go
@@ -5,11 +5,13 @@ import (
 	"io/ioutil"
 	"path"
 	"sort"
+	"strings"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
 	"gopkg.in/op/go-logging.v1"
 
+	"build"
 	"core"
 	"grpcutil"
 	pb "test/proto/remote"
@@ -75,25 +77,26 @@ func (w *worker) ExecuteTest(ctx context.Context, req *pb.RemoteTestRequest) (*p
 	defer func() {
 		w.Running = false
 	}()
-	code, stderr, err := w.runTest(ctx, req)
+	stderr, err := w.runTest(ctx, req)
+	resp := &pb.RemoteTestResponse{
+		Success: err == nil,
+		Results: w.findResults(req),
+		Stderr:  stderr,
+	}
 	if err != nil {
-		return &pb.RemoteTestResponse{Error: err.Error()}, nil
+		resp.Error = err.Error()
 	}
-	return &pb.RemoteTestResponse{
-		ExitCode: int32(code),
-		Results:  w.findResults(req),
-		Stderr:   stderr,
-	}
+	return resp, nil
 }
 
 // runTest runs a single test.
-func (w *worker) runTest(ctx context.Context, req *pb.RemoteTestRequest) (int, []byte, error) {
+func (w *worker) runTest(ctx context.Context, req *pb.RemoteTestRequest) ([]byte, error) {
 	if err := os.RemoveAll(w.Dir); err != nil {
-		return 0, nil, err
+		return nil, err
 	} else if err := os.MkdirAll(w.Dir, os.ModeDir|0755); err != nil {
-		return 0, nil, err
+		return nil, err
 	} else if err := w.setupFiles(req.Outputs, req.Data); err != nil {
-		return 0, nil, err
+		return nil, err
 	}
 	// Using a default state here is not really accurate but is the best we can do for now.
 	// TODO(peterebden): Find some way of specifying this on the worker.
@@ -110,8 +113,14 @@ func (w *worker) runTest(ctx context.Context, req *pb.RemoteTestRequest) (int, [
 	}
 	state := core.DefaultBuildState()
 	state.NeedCoverage = req.Coverage
+	cmd := build.ReplaceTestSequences(state, target, target.GetTestCommand(state))
 	env := core.TestEnvironment(state, target, w.Dir)
-
+	timeout := time.Duration(req.Timeout) * time.Second
+	log.Notice("Running test %s\nENVIRONMENT:\n%s\n%s", target.Label, strings.Join(env, "\n"), cmd)
+	// TODO(peterebden): The second-to-last argument allows sandboxing. We should probably allow
+	//                   configuring that on the build agent.
+	_, stderr, err := core.ExecWithTimeoutShellStdStreams(state, target, w.Dir, env, timeout, timeout, false, cmd, false, false)
+	return stderr, err
 }
 
 // setupFiles creates the files in the test directory.
