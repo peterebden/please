8fbba7b21e794b668ac08e31785e697ed36973ea
Peter Ebden <peter.ebden@gmail.com>
2018-12-01 11:05:40 +0000

    Adding a test on client side

--- src/remote/fsclient/client.go
+++ src/remote/fsclient/client.go
@@ -5,6 +5,7 @@ package fsclient
 import (
 	"context"
 	"fmt"
+	"io"
 	"sort"
 	"sync"
 	"time"
@@ -12,11 +13,10 @@ import (
 	"github.com/cespare/xxhash"
 	"github.com/hashicorp/go-multierror"
 	"golang.org/x/sync/errgroup"
-	"google.golang.org/grpc"
 	"gopkg.in/op/go-logging.v1"
 
 	"grpcutil"
-	pb "remote/proto/fs"
+	pb "src/remote/proto/fs"
 )
 
 var log = logging.MustGetLogger("fsclient")
@@ -29,9 +29,9 @@ const chunkSize = 32 * 1024
 // NewClient creates and returns a new client based on the given URL touchpoints that it can
 // initialise from.
 // N.B. Errors are not returned here since the initialisation is done asynchronously.
-func NewClient(urls []string) (Client, error) {
-	c := &client{}
-	go c.Init() // Begin initialisation eagerly
+func NewClient(urls []string) Client {
+	c := &client{urls: urls}
+	go c.Init()
 	return c
 }
 
@@ -40,41 +40,34 @@ type client struct {
 	nodes    []*node
 	ranges   []hashRange
 	initOnce sync.Once
+	urls     []string
 }
 
-func (c *client) Get(filenames []string, hash []byte) ([]io.ReadCloser, error) {
+func (c *client) Get(filenames []string, hash []byte) ([]io.Reader, error) {
+	c.Init()
 	h := xxhash.Sum64(hash)
-	nodes, err := c.nodes(h)
+	nodes, err := c.findNodes(h)
 	if err != nil {
 		return nil, err
 	}
 	// Fast path if we get a single file
 	if len(filenames) == 1 {
 		r, err := c.getFile(h, nodes, filenames[0])
-		return []io.ReadCloser{r}, err
+		return []io.Reader{r}, err
 	}
 
 	var g errgroup.Group
-	ret := make([]io.ReadCloser, len(filenames))
+	ret := make([]io.Reader, len(filenames))
 	for i, filename := range filenames {
 		i := i
 		filename := filename
 		g.Go(func() error {
-			r, err := c.getFile(nodes, filename)
+			r, err := c.getFile(h, nodes, filename)
 			ret[i] = r
 			return err
 		})
 	}
-	if err := g.Wait(); err != nil {
-		// Close anything that we got back
-		for _, r := range ret {
-			if r != nil {
-				r.Close()
-			}
-		}
-		return nil, err
-	}
-	return ret, nil
+	return ret, g.Wait()
 }
 
 // getFile retrieves a single file from the remote cluster.
@@ -84,7 +77,7 @@ func (c *client) getFile(hash uint64, nodes []*node, filename string) (io.Reader
 	var e error
 	for _, n := range nodes {
 		n.Init()
-		stream, err := n.Client.Get(&pb.GetRequest{Hash: hash, Name: filename, ChunkSize: chunkSize})
+		stream, err := n.Client.Get(context.Background(), &pb.GetRequest{Hash: hash, Name: filename, ChunkSize: chunkSize})
 		if err != nil {
 			e = multierror.Append(err, e)
 			continue
@@ -95,16 +88,18 @@ func (c *client) getFile(hash uint64, nodes []*node, filename string) (io.Reader
 }
 
 func (c *client) Put(filename string, hash []byte, content io.Reader) error {
+	c.Init()
 	h := xxhash.Sum64(hash)
-	nodes, err := c.nodes(h)
+	nodes, err := c.findNodes(h)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	// Try each of the nodes until we find one that works.
 	var e error
+nodeloop:
 	for _, node := range nodes {
-		n.Init()
-		stream, err := n.Client.Put(&pb.PutRequest{Hash: h, Name: filename})
+		node.Init()
+		stream, err := node.Client.Put(context.Background())
 		if err != nil {
 			e = multierror.Append(err, e)
 			continue
@@ -113,12 +108,12 @@ func (c *client) Put(filename string, hash []byte, content io.Reader) error {
 		for {
 			n, err := content.Read(buf)
 			if n > 0 {
-				if err := stream.Send(&pb.PutRequest{Chunk: buf[:n]}); err != nil {
+				if err := stream.Send(&pb.PutRequest{Hash: h, Name: filename, Chunk: buf[:n]}); err != nil {
 					// This is most likely their error rather than ours, so we try another
 					// replica (this makes us robust to one of them going down during transfer)
 					log.Warning("Error sending file to remote storage: %s", err)
 					e = multierror.Append(err, e)
-					continue
+					continue nodeloop
 				}
 			}
 			if err != nil {
@@ -145,7 +140,7 @@ func (c *client) Init() {
 func (c *client) init() {
 	var e error
 	// Try the URLs one by one until we find one that works.
-	for _, url := range urls {
+	for _, url := range c.urls {
 		client := pb.NewRemoteFSClient(grpcutil.Dial(url))
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
@@ -157,17 +152,16 @@ func (c *client) init() {
 		c.setupTopology(client, resp)
 		return
 	}
-	log.Fatalf("Failed to connect to remote FS server: %s", err)
+	log.Fatalf("Failed to connect to remote FS server: %s", e)
 }
 
 // setupTopology sets up the definition of the hash ranges of the remote.
 func (c *client) setupTopology(client pb.RemoteFSClient, info *pb.InfoResponse) {
-	c.nodes = make([]node, len(info.Nodes))
-	for i, n := range info.Nodes {
+	c.nodes = make([]*node, len(info.Node))
+	for i, n := range info.Node {
 		c.nodes[i] = &node{
 			Address: n.Address,
 			Name:    n.Name,
-			Ranges:  make([]hashRange, len(n.Ranges)),
 		}
 		if n.Address == info.ThisNode.Address {
 			c.nodes[i].Client = client
@@ -187,10 +181,10 @@ func (c *client) setupTopology(client pb.RemoteFSClient, info *pb.InfoResponse) 
 	})
 }
 
-// nodes returns all the nodes that can handle a particular hash.
-func (c *client) nodes(h uint64) ([]*node, error) {
+// findNodes returns all the nodes that can handle a particular hash.
+func (c *client) findNodes(h uint64) ([]*node, error) {
 	start := sort.Search(len(c.ranges), func(i int) bool { return c.ranges[i].Start < h })
-	ret := make([]*node, 0, 4) // slightly arbitrary size assuming a few replicas & a little overlap
+	ret := []*node{}
 	for _, r := range c.ranges[start:] {
 		if h > r.End {
 			break
@@ -200,7 +194,7 @@ func (c *client) nodes(h uint64) ([]*node, error) {
 	if len(ret) == 0 {
 		// We should not normally get here - it implies we were given an incomplete set of nodes
 		// that do not cover the full hash space (or possibly so many are down that there's a hole)
-		return fmt.Errorf("Cannot find range for hash %x (broken ring?)", h)
+		return nil, fmt.Errorf("Cannot find range for hash %x (broken ring?)", h)
 	}
 	// Sort the nodes into priority order.
 	// Right now this is just prioritised by whether they're initialised but later we might
---
+++ src/remote/fsclient/fsclient_test.go
@@ -0,0 +1,118 @@
+package fsclient
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"grpcutil"
+	pb "src/remote/proto/fs"
+)
+
+func TestClient(t *testing.T) {
+	s1 := grpc.NewServer()
+	srv1 := &server{files: map[key][]byte{}}
+	pb.RegisterRemoteFSServer(s1, srv1)
+	lis1 := grpcutil.SetupServer(s1, 0)
+	go s1.Serve(lis1)
+
+	s2 := grpc.NewServer()
+	srv2 := &server{files: map[key][]byte{}}
+	pb.RegisterRemoteFSServer(s2, srv2)
+	lis2 := grpcutil.SetupServer(s2, 0)
+	go s2.Serve(lis2)
+
+	info = &pb.InfoResponse{
+		Node: []*pb.Node{
+			{
+				Address: lis1.Addr().String(),
+				Name:    "node-1",
+				Ranges: []*pb.Range{
+					{Start: 0, End: 999},
+					{Start: 2000, End: 2999},
+					{Start: 4000, End: 4999},
+					{Start: 6000, End: math.MaxUint64},
+				},
+			},
+			{
+				Address: lis2.Addr().String(),
+				Name:    "node-2",
+				Ranges: []*pb.Range{
+					{Start: 1000, End: 1999},
+					{Start: 3000, End: 3999},
+					{Start: 5000, End: 5999},
+				},
+			},
+		},
+	}
+	info.ThisNode = info.Node[0]
+
+	client := NewClient([]string{lis1.Addr().String()})
+	// Make this big enough so it exercises the chunking code properly.
+	content := bytes.Repeat([]byte("testing"), 100000)
+	hash := []byte("wevs")
+	const name1 = "test1.txt"
+	const name2 = "test2.txt"
+	err := client.Put(name1, hash, bytes.NewReader(content))
+	assert.NoError(t, err)
+	err = client.Put(name2, hash, bytes.NewReader(content))
+	assert.NoError(t, err)
+
+	rs, err := client.Get([]string{name1, name2}, hash)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(rs))
+	b1, err := ioutil.ReadAll(rs[0])
+	assert.NoError(t, err)
+	b2, err := ioutil.ReadAll(rs[1])
+	assert.NoError(t, err)
+	assert.EqualValues(t, content, b1)
+	assert.EqualValues(t, content, b2)
+}
+
+// server implements a fake in-memory server for testing.
+type server struct {
+	files map[key][]byte
+}
+
+type key struct {
+	Hash uint64
+	Name string
+}
+
+func (s *server) Info(ctx context.Context, req *pb.InfoRequest) (*pb.InfoResponse, error) {
+	return info, nil
+}
+
+func (s *server) Get(req *pb.GetRequest, stream pb.RemoteFS_GetServer) error {
+	f, present := s.files[key{Hash: req.Hash, Name: req.Name}]
+	if !present {
+		return status.Errorf(codes.NotFound, "file not found")
+	}
+	for len(f) > 0 {
+		stream.Send(&pb.GetResponse{Chunk: f[:req.ChunkSize]})
+		f = f[req.ChunkSize:]
+	}
+	return nil
+}
+
+func (s *server) Put(stream pb.RemoteFS_PutServer) error {
+	req, _ := stream.Recv()
+	k := key{Hash: req.Hash, Name: req.Name}
+	f := req.Chunk
+	var err error
+	for err != nil {
+		req, err = stream.Recv()
+		f = append(f, req.Chunk...)
+	}
+	s.files[k] = f
+	return nil
+}
+
+var info *pb.InfoResponse
