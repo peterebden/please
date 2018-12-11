6277158380517a49a1de30744a2eef501bf86692
Peter Ebden <peter.ebden@gmail.com>
2018-12-11 18:51:42 +0000

    start of rebrand

--- docs/template.go
+++ docs/template.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"hash/adler32"
 	"io/ioutil"
 	"os"
 	"path"
 	"text/template"
 )
 
-var iconImages = map[string]string{
-	"advanced.html":         "teal4.png",
-	"acknowledgements.html": "teal2.png",
-	"basics.html":           "teal1.png",
-	"build_rules.html":      "teal1.png",
-	"cache.html":            "teal3.png",
-	"commands.html":         "teal6.png",
-	"config.html":           "teal5.png",
-	"cross_compiling.html":  "teal5.png",
-	"dependencies.html":     "teal3.png",
-	"faq.html":              "teal4.png",
-	"intermediate.html":     "teal2.png",
-	"language.html":         "teal5.png",
-	"lexicon.html":          "teal1.png",
-	"pleasings.html":        "teal3.png",
-	"quickstart.html":       "teal6.png",
-	"error.html":            "teal4.png",
+var shapes = []string{
+	"circle",
+	"hexagon",
+	"pentagon",
+	"square",
+	"triangle",
+}
+
+var colours = []string{
+	"b", // blue
+	"c", // cyan
+	"g", // green
+	"p", // pink
+	"r", // red
+	"t", // turquoise
+	"v", // violet
+	"y", // yellow
 }
 
 var pageTitles = map[string]string{
@@ -55,6 +55,7 @@ func mustRead(filename string) string {
 func main() {
 	filename := os.Args[2]
 	basename := path.Base(filename)
+	basenameIndex := int(adler32.Checksum([]byte(basename)))
 	funcs := template.FuncMap{
 		"menuItem": func(s string) string {
 			if basename[:len(basename)-5] == s {
@@ -62,21 +63,19 @@ func main() {
 			}
 			return ""
 		},
-		"iconImage": func() string {
-			if img := iconImages[basename]; img != "" {
-				return fmt.Sprintf(`<img src="images/%s">`, img)
-			}
-			return ""
+		"shape": func(i int) string {
+			return shapes[(basenameIndex+i)%len(shapes)]
 		},
 	}
 	data := struct {
 		Title, Header, Contents string
-		Player                  bool
+		Player, IsIndex         bool
 	}{
 		Title:    pageTitles[basename],
 		Header:   mustRead(os.Args[1]),
 		Contents: mustRead(filename),
 		Player:   basename == "faq.html",
+		IsIndex:  basename == "index.html",
 	}
 
 	tmpl := template.Must(template.New("tmpl").Funcs(funcs).Parse(mustRead(os.Args[1])))
