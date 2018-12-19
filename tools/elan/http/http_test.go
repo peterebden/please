46593646c4ef2a0015da3d499b025dc724aa0cda
Peter Ebden <peter.ebden@gmail.com>
2018-12-19 08:12:47 +0000

    fix test

--- tools/elan/http/http_test.go
+++ tools/elan/http/http_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestSVGPath(t *testing.T) {
-	const expected = "M400,100 L400,0 A400,400 1 0,1 800.00000,400.00000 L700.00000,400.00000 A400,400 1 0,0 400,100"
+	const expected = "M400,100 L400,0 A400,400 1 0,1 800.00000,400.00000 L700.00000,400.00000 A300,300 1 0,0 400,100"
 	actual := svgPath(0, 4611686018427387904)
 	assert.Equal(t, expected, actual)
 	actual = svgPath(4611686018427387904, 9223372036854775808)
