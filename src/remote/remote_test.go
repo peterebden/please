4c1096edefb025bdfd3c9dd68a07bff95a13ac26
Peter Ebden <peter.ebden@gmail.com>
2019-07-16 19:25:55 +0600

    Adding noddy server implementation to test against

--- src/remote/remote_test.go
+++ src/remote/remote_test.go
@@ -2,16 +2,21 @@ package remote
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	rpcstatus "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
 	"github.com/bazelbuild/remote-apis/build/bazel/semver"
 	"github.com/thought-machine/please/src/core"
+	bs "google.golang.org/genproto/googleapis/bytestream"
 )
 
 func TestInit(t *testing.T) {
@@ -52,6 +57,9 @@ func newClient() *Client {
 type testServer struct {
 	DigestFunction                []pb.DigestFunction_Value
 	LowApiVersion, HighApiVersion semver.SemVer
+	actionResults                 map[string]*pb.ActionResult
+	blobs                         map[string][]byte
+	bytestreams                   map[string][]byte
 }
 
 func (s *testServer) GetCapabilities(ctx context.Context, req *pb.GetCapabilitiesRequest) (*pb.ServerCapabilities, error) {
@@ -75,6 +83,139 @@ func (s *testServer) Reset() {
 	}
 	s.LowApiVersion = semver.SemVer{Major: 2}
 	s.HighApiVersion = semver.SemVer{Major: 2, Minor: 1}
+	s.actionResults = map[string]*pb.ActionResult{}
+	s.blobs = map[string][]byte{}
+	s.bytestreams = map[string][]byte{}
+}
+
+func (s *testServer) GetActionResult(ctx context.Context, req *pb.GetActionResultRequest) (*pb.ActionResult, error) {
+	ar, present := s.actionResults[req.ActionDigest.Hash]
+	if !present {
+		return nil, status.Errorf(codes.NotFound, "action result not found")
+	}
+	return ar, nil
+}
+
+func (s *testServer) UpdateActionResult(ctx context.Context, req *pb.UpdateActionResultRequest) (*pb.ActionResult, error) {
+	s.actionResults[req.ActionDigest.Hash] = req.ActionResult
+	return req.ActionResult, nil
+}
+
+func (s *testServer) FindMissingBlobs(ctx context.Context, req *pb.FindMissingBlobsRequest) (*pb.FindMissingBlobsResponse, error) {
+	resp := &pb.FindMissingBlobsResponse{}
+	for _, d := range req.BlobDigests {
+		if _, present := s.blobs[d.Hash]; !present {
+			resp.MissingBlobDigests = append(resp.MissingBlobDigests, d)
+		}
+	}
+	return resp, nil
+}
+
+func (s *testServer) BatchUpdateBlobs(ctx context.Context, req *pb.BatchUpdateBlobsRequest) (*pb.BatchUpdateBlobsResponse, error) {
+	resp := &pb.BatchUpdateBlobsResponse{
+		Responses: make([]*pb.BatchUpdateBlobsResponse_Response, len(req.Requests)),
+	}
+	for i, r := range req.Requests {
+		resp.Responses[i].Status = &rpcstatus.Status{}
+		if len(r.Data) != int(r.Digest.SizeBytes) {
+			resp.Responses[i].Status.Code = int32(codes.InvalidArgument)
+			resp.Responses[i].Status.Message = fmt.Sprintf("Blob sizes do not match (%d / %d)", len(r.Data), r.Digest.SizeBytes)
+		} else {
+			s.blobs[r.Digest.Hash] = r.Data
+		}
+	}
+	return resp, nil
+}
+
+func (s *testServer) BatchReadBlobs(ctx context.Context, req *pb.BatchReadBlobsRequest) (*pb.BatchReadBlobsResponse, error) {
+	resp := &pb.BatchReadBlobsResponse{
+		Responses: make([]*pb.BatchReadBlobsResponse_Response, len(req.Digests)),
+	}
+	for i, d := range req.Digests {
+		resp.Responses[i].Status = &rpcstatus.Status{}
+		resp.Responses[i].Digest = d
+		if data, present := s.blobs[d.Hash]; present {
+			resp.Responses[i].Data = data
+		} else {
+			resp.Responses[i].Status.Code = int32(codes.NotFound)
+			resp.Responses[i].Status.Message = fmt.Sprintf("Blob %s not found", d.Hash)
+		}
+	}
+	return resp, nil
+}
+
+func (s *testServer) GetTree(*pb.GetTreeRequest, pb.ContentAddressableStorage_GetTreeServer) error {
+	return status.Errorf(codes.Unimplemented, "GetTree not implemented for test")
+}
+
+func (s *testServer) Read(req *bs.ReadRequest, srv bs.ByteStream_ReadServer) error {
+	b, present := s.blobs[req.ResourceName]
+	if !present {
+		return status.Errorf(codes.NotFound, "bytestream %s not found", req.ResourceName)
+	} else if req.ReadOffset < 0 || req.ReadOffset > int64(len(b)) {
+		return status.Errorf(codes.OutOfRange, "invalid offset for bytestream %s, was %d, must be [0-%d]", req.ResourceName, req.ReadOffset, len(b))
+	} else if req.ReadLimit < 0 {
+		return status.Errorf(codes.OutOfRange, "negative ReadLimit")
+	}
+	b = b[req.ReadOffset:]
+	if req.ReadLimit != 0 && req.ReadLimit < int64(len(b)) {
+		b = b[:req.ReadLimit]
+	}
+	// Now stream these back a bit at a time.
+	for i := 0; i < len(b); i += 1024 {
+		n := i + 1024
+		if n > len(b) {
+			n = len(b)
+		}
+		if err := srv.Send(&bs.ReadResponse{Data: b[i:n]}); err != nil {
+			return fmt.Errorf("ByteStream::Read error: %s", err)
+		}
+	}
+	return nil
+}
+
+func (s *testServer) Write(srv bs.ByteStream_WriteServer) error {
+	req, err := srv.Recv()
+	if err != nil {
+		return fmt.Errorf("ByteStream::Write error: %s", err)
+	} else if req.ResourceName == "" {
+		return status.Errorf(codes.InvalidArgument, "missing ResourceName")
+	}
+	name := req.ResourceName
+	b := s.bytestreams[name]
+	for {
+		if req.WriteOffset != int64(len(b)) {
+			return status.Errorf(codes.InvalidArgument, "incorrect WriteOffset (was %d, should be %d)", req.WriteOffset, len(b))
+		}
+		b = append(b, req.Data...)
+		if req.FinishWrite {
+			s.blobs[name] = b
+			delete(s.bytestreams, name)
+			break
+		}
+		s.bytestreams[name] = b
+		req, err = srv.Recv()
+		if err != nil {
+			return fmt.Errorf("ByteStream::Write error: %s", err)
+		}
+	}
+	return srv.SendAndClose(&bs.WriteResponse{
+		CommittedSize: int64(len(b)),
+	})
+}
+
+func (s *testServer) QueryWriteStatus(ctx context.Context, req *bs.QueryWriteStatusRequest) (*bs.QueryWriteStatusResponse, error) {
+	if b, present := s.blobs[req.ResourceName]; present {
+		return &bs.QueryWriteStatusResponse{
+			CommittedSize: int64(len(b)),
+			Complete:      true,
+		}, nil
+	} else if b, present := s.bytestreams[req.ResourceName]; present {
+		return &bs.QueryWriteStatusResponse{
+			CommittedSize: int64(len(b)),
+		}, nil
+	}
+	return nil, status.Errorf(codes.NotFound, "resource %s not found", req.ResourceName)
 }
 
 var server = &testServer{}
@@ -87,6 +228,9 @@ func TestMain(m *testing.M) {
 	}
 	s := grpc.NewServer()
 	pb.RegisterCapabilitiesServer(s, server)
+	pb.RegisterActionCacheServer(s, server)
+	pb.RegisterContentAddressableStorageServer(s, server)
+	bs.RegisterByteStreamServer(s, server)
 	go s.Serve(lis)
 	code := m.Run()
 	s.Stop()
