b6362a1d91102db616f9871aeaad5f86587ae8b0
Peter Ebden <peter.ebden@gmail.com>
2021-05-09 18:51:31 +0100

    Write the world's worst hashmap.

--- src/core/cmap_targets.go
+++ src/core/cmap_targets.go
@@ -29,7 +29,7 @@ func newTargetMap() *targetMap {
 		shards: make([]*targetLMap, shardCount),
 	}
 	for i := range cm.shards {
-		cm.shards[i] = newTargetLMapSize(shardCount)
+		cm.shards[i] = newTargetLMap()
 	}
 	return cm
 }
@@ -38,13 +38,13 @@ func newTargetMap() *targetMap {
 // It returns true if the item was inserted, false if it already existed (in which case it won't be inserted)
 func (cm *targetMap) Set(key BuildLabel, val *BuildTarget) bool {
 	h := hashBuildLabel(key)
-	return cm.shards[h&shardMask].Set(key, val)
+	return cm.shards[h&shardMask].Set(h, key, val)
 }
 
 // GetOK is the equivalent of `val, ok := map[key]`.
 func (cm *targetMap) GetOK(key BuildLabel) (val *BuildTarget, ok bool) {
 	h := hashBuildLabel(key)
-	return cm.shards[h&shardMask].GetOK(key)
+	return cm.shards[h&shardMask].GetOK(h, key)
 }
 
 // Values returns a slice of all the current values in the map.
@@ -62,47 +62,49 @@ func hashBuildLabel(key BuildLabel) uint32 {
 	return hashers.Fnv32(key.Subrepo) ^ hashers.Fnv32(key.PackageName) ^ hashers.Fnv32(key.Name)
 }
 
-// targetLMap is a simple sync.RWMutex locked map.
+// targetLMap is an individually locked map, which was once implemented over a builtin map but is no longer.
 // Used by targetMap internally for sharding.
 type targetLMap struct {
-	m map[BuildLabel]*BuildTarget
+	s [][]*BuildTarget
 	l sync.RWMutex
 }
 
-// newTargetLMapSize is the equivalent of `m := make(map[BuildLabel]*BuildTarget, cap)`
-func newTargetLMapSize(cap int) *targetLMap {
+func newTargetLMap() *targetLMap {
 	return &targetLMap{
-		m: make(map[BuildLabel]*BuildTarget, cap),
+		s: make([][]*BuildTarget, shardCount),
 	}
 }
 
 // Set is the equivalent of `map[key] = val`.
 // It returns true if the item was inserted, false if it already existed (in which case it won't be inserted)
-func (lm *targetLMap) Set(key BuildLabel, v *BuildTarget) bool {
+func (lm *targetLMap) Set(hash uint32, key BuildLabel, v *BuildTarget) bool {
+	k := hash & shardMask
 	lm.l.Lock()
 	defer lm.l.Unlock()
-	if _, present := lm.m[key]; present {
-		return false
-	}
-	lm.m[key] = v
+	lm.s[k] = append(lm.s[k], v)
 	return true
 }
 
 // GetOK is the equivalent of `val, ok := map[key]`.
-func (lm *targetLMap) GetOK(key BuildLabel) (*BuildTarget, bool) {
+func (lm *targetLMap) GetOK(hash uint32, key BuildLabel) (*BuildTarget, bool) {
+	k := hash & shardMask
 	lm.l.RLock()
 	defer lm.l.RUnlock()
-	v, ok := lm.m[key]
-	return v, ok
+	for _, t := range lm.s[k] {
+		if t.Label == key {
+			return t, true
+		}
+	}
+	return nil, false
 }
 
 // Values returns a copy of all the values currently in the map.
 func (lm *targetLMap) Values() []*BuildTarget {
 	lm.l.RLock()
 	defer lm.l.RUnlock()
-	ret := make([]*BuildTarget, 0, len(lm.m))
-	for _, v := range lm.m {
-		ret = append(ret, v)
+	ret := []*BuildTarget{}
+	for _, s := range lm.s {
+		ret = append(ret, s...)
 	}
 	return ret
 }
