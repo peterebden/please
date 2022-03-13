56df8b06017faa9a1206f254ae1844ca16dec47a
Peter Ebden <peter.ebden@gmail.com>
2022-03-13 12:43:20 +0000

    theoretically maybe works

--- src/core/config_flags.go
+++ src/core/config_flags.go
@@ -1,9 +1,11 @@
 package core
 
 import (
-	"github.com/thought-machine/go-flags"
-
+	"os"
+	"reflect"
 	"strings"
+
+	"github.com/thought-machine/go-flags"
 )
 
 // AttachAliasFlags attaches the alias flags to the given flag parser.
@@ -32,9 +34,9 @@ func (config *Configuration) AttachAliasFlags(parser *flags.Parser) bool {
 }
 
 // addSubcommands attaches a series of subcommands to the given command.
-func addSubcommands(cmd *flags.Command, subcommands []string, positionalLabels bool) {
+func addSubcommands(cmd *flags.Command, subcommands []string, positionalLabels bool, requiredLabel string) {
 	if len(subcommands) > 0 && cmd != nil {
-		addSubcommands(addSubcommand(cmd, subcommands[0], "", positionalLabels), subcommands[1:], positionalLabels)
+		addSubcommands(addSubcommand(cmd, subcommands[0], "", positionalLabels, requiredLabel), subcommands[1:], positionalLabels, requiredLabel)
 	}
 }
 
@@ -46,12 +48,38 @@ func addSubcommand(cmd *flags.Command, subcommand, desc string, positionalLabels
 	}
 	var data interface{} = &struct{}{}
 	if positionalLabels {
-		data = &struct {
-			Args struct {
-				Target []BuildLabel `positional-arg-name:"target" description:"Build targets"`
-			} `positional-args:"true"`
-		}{}
+		label := reflect.StructOf([]reflect.StructField{{
+			Name: "Label",
+			Type: reflect.TypeOf(aliasLabel{}),
+			Tag:  reflect.StructTag(`required-label:"` + requiredLabel + `"`),
+		}})
+		if _, ok := reflect.New(label).Interface().(flags.Completer); !ok {
+			log.Fatalf("Failed to create a valid Completer")
+		}
+		args := reflect.StructOf([]reflect.StructField{{
+			Name: "Target",
+			Type: reflect.SliceOf(label),
+			Tag:  reflect.StructTag(`positional-arg-name:"target" description:"Build targets"`),
+		}})
+		d := reflect.StructOf([]reflect.StructField{{
+			Name: "Args",
+			Type: args,
+			Tag:  reflect.StructTag(`positional-args:"true"`),
+		}})
+		data = reflect.New(d).Interface()
 	}
 	newCmd, _ := cmd.AddCommand(subcommand, desc, desc, data)
 	return newCmd
+}
+
+// An aliasLabel is used for completing build labels on aliases. Some fiddling is needed
+// since completion is a property of the type, rather than of the value.
+type aliasLabel struct {
+	Label         BuildLabel
+	RequiredLabel string
+}
+
+func (label aliasLabel) Complete(match string) []flags.Completion {
+	os.Setenv("PLZ_COMPLETE_LABEL", label.RequiredLabel)
+	return label.Label.Complete(match)
 }
